b64dd2f5fe80317f31546aaa99c27ab9c76d7d55
Ben Thomas <44470695+Xiphias5985@users.noreply.github.com>
2021-09-19 21:52:35 +1000

    Restored original ENV reference

--- initdb.js
+++ initdb.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.DB_URL_2;
+const uri = process.env.DB_URL;
 const client = new MongoClient(uri);
 
 async function main() {
