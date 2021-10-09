76910adc24f40939071b2dc9d2aa2deb47760a14
dhruvnargund <nargund36@gmail.com>
2021-10-09 13:13:02 +0530

    added username in signup

--- services/signUpService.js
+++ services/signUpService.js
@@ -1,8 +1,9 @@
 let client = require("../initdb");
 const path = require("path");
 
-const getSignUp = (req, res) => {
-    res.render(path.join(__dirname, '../public', 'signUp.html'));
+
+const getSignUp = (res) => {
+    res.sendFile(path.join(__dirname, '../public', 'signUp.html'));
 }
 
 module.exports = {
