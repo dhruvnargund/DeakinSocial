e0ae6d91ca83550c273e3770658c7315ff9307c2
Ben Thomas <bathomas5985@gmail.com>
2021-10-10 22:56:10 +1100

    Updated profile controll & service hc

--- app/services/profileService.js
+++ app/services/profileService.js
@@ -12,12 +12,28 @@ const getAllProfiles = (req, res) => {
     .catch((err) => console.log("Error:", err));
 }
 
-const getProfile = (req, res, JWT) => {
+const getProfile = (req, res) => {
     console.log('Hit the specific profile service');
-    User.findOne({username: req.params.id}).then((result) => res.render('profile', {
-        owner: req.params.id,
-        result
-    })).catch((err) => console.log("Error:", err));
-}
+    if (req.params.id) {
+        var id = req.params.id;
+    } else {
+        id = 'dnargund';
+    }
+    Promise.all([
+        User.findOne({username: id}),
+        Post.find({username: id})
+        ])
+        .then(results => {
+            const user = results[0];
+            const posts = results [1];
+            res.render('profile', {
+                user,
+                posts
+            })    
+    })
+    .catch(err=>{
+    console.error("Something went wrong",err);
+      })
+    }
 
 module.exports = { getAllProfiles, getProfile }
